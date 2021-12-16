f3055ed36af00088680529290d5517879f685579
Eric Long <hackereric@outlook.com>
2021-12-16 22:39:36 +0800

    Finalize `create_service` Make `Source` opaque

--- hive-core/src/lib.rs
+++ hive-core/src/lib.rs
@@ -9,18 +9,34 @@ mod service;
 mod source;
 
 pub use error::{Error, HiveResult};
+pub use service::{Service, ServiceGuard};
 
 use lua::Sandbox;
 use object_pool::Pool;
+use service::ServicePool;
+use source::Source;
 
 pub struct Hive {
   sandbox_pool: Pool<Sandbox>,
+  service_pool: ServicePool,
 }
 
 impl Hive {
   pub fn new() -> HiveResult<Self> {
     Ok(Self {
       sandbox_pool: Pool::with_capacity(8, Sandbox::new)?,
+      service_pool: ServicePool::new(),
     })
+  }
+
+  pub async fn create_service(
+    &self,
+    name: impl Into<String>,
+    source: Source,
+  ) -> HiveResult<Service> {
+    self
+      .service_pool
+      .create_service(&self.sandbox_pool, name, source)
+      .await
   }
 }
--- hive-core/src/source.rs
+++ hive-core/src/source.rs
@@ -2,17 +2,26 @@ use std::sync::Arc;
 
 /// Shared, immutable source code storage.
 #[derive(Debug, Clone)]
-pub enum Source {
-  Single(Arc<str>), // Multiple(Archive)
+pub struct Source {
+  inner: SourceInner,
+}
+
+#[derive(Debug, Clone)]
+enum SourceInner {
+  Single(Arc<[u8]>),
 }
 
 impl Source {
+  pub fn new_single(content: impl Into<Arc<[u8]>>) -> Self {
+    Self {
+      inner: SourceInner::Single(content.into()),
+    }
+  }
+
   pub fn get(&self, path: &str) -> Option<&[u8]> {
     let segments: Vec<_> = path.split("/").filter(|x| !x.is_empty()).collect();
-    match self {
-      Self::Single(main) if segments.len() == 1 && segments[0] == "main.lua" => {
-        Some(main.as_bytes())
-      }
+    match &self.inner {
+      SourceInner::Single(main) if segments.len() == 1 && segments[0] == "main.lua" => Some(&main),
       _ => None,
     }
   }
