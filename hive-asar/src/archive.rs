531bf12df48fc884129612fd1e200a0ea9657064
Eric Long <hackereric@outlook.com>
2022-02-05 17:43:46 +0800

    `impl Vfs for Archive<fs::File>`

--- hive-asar/src/archive.rs
+++ hive-asar/src/archive.rs
@@ -146,3 +146,76 @@ impl<R: AsyncRead + AsyncSeek + Unpin> AsyncRead for File<R> {
 }
 
 // TODO: impl AsyncSeek for File
+
+#[cfg(feature = "vfs")]
+mod vfs_impl {
+  use super::*;
+  use async_trait::async_trait;
+  use futures::stream::BoxStream;
+  use hive_vfs::{FileMode, Metadata, Vfs};
+
+  #[async_trait]
+  impl Vfs for Archive<fs::File> {
+    type File = File<fs::File>;
+
+    async fn open_file<'a>(&'a self, path: &str, mode: FileMode) -> hive_vfs::Result<Self::File>
+    where
+      Self::File: 'a,
+    {
+      if let FileMode::Read = mode {
+        Ok(self.fs_read(path).await?)
+      } else {
+        Err(hive_vfs::Error::MethodNotAllowed)
+      }
+    }
+
+    async fn read_dir(&self, path: &str) -> hive_vfs::Result<BoxStream<hive_vfs::Result<String>>> {
+      let segments = split_path(path);
+      let entry = self.header.search_segments(&segments);
+      match entry {
+        Some(Entry::Directory(d)) => {
+          let x = (d.files.iter())
+            .map(|(name, _entry)| {
+              Ok(
+                segments
+                  .iter()
+                  .copied()
+                  .chain(std::iter::once(&**name))
+                  .fold(String::new(), |b, x| b + "/" + x),
+              )
+            })
+            .collect::<Vec<_>>();
+          Ok(Box::pin(futures::stream::iter(x)))
+        }
+        Some(_) => Err(io::Error::new(io::ErrorKind::Other, "not a directory").into()),
+        None => Err(io::Error::from(io::ErrorKind::NotFound).into()),
+      }
+    }
+
+    async fn metadata(&self, path: &str) -> hive_vfs::Result<Metadata> {
+      let entry = self.header.search_segments(&split_path(path));
+      match entry {
+        Some(Entry::Directory(_)) => Ok(Metadata::Directory),
+        Some(Entry::File(f)) => Ok(Metadata::File { len: f.size }),
+        None => Err(io::Error::from(io::ErrorKind::NotFound).into()),
+      }
+    }
+
+    async fn exists(&self, path: &str) -> hive_vfs::Result<bool> {
+      let entry = self.header.search_segments(&split_path(path));
+      Ok(entry.is_some())
+    }
+
+    async fn create_dir(&self, _path: &str) -> hive_vfs::Result<()> {
+      Err(hive_vfs::Error::MethodNotAllowed)
+    }
+
+    async fn remove_file(&self, _path: &str) -> hive_vfs::Result<()> {
+      Err(hive_vfs::Error::MethodNotAllowed)
+    }
+
+    async fn remove_dir(&self, _path: &str) -> hive_vfs::Result<()> {
+      Err(hive_vfs::Error::MethodNotAllowed)
+    }
+  }
+}
