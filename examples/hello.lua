b75491063217ae9488e1f5b4c17c97028b399db4
Eric Long <i@hack3r.moe>
2022-06-29 18:34:50 +0800

    Modify examples

--- examples/hello.lua
+++ examples/hello.lua
@@ -1,19 +1,7 @@
-local json = require "json"
-
-function hive.start()
-  names = json.array {}
-end
-
 local function hello(req)
   local name = req.params.name or "world"
-  table.insert(names, name)
   return { greeting = "Hello, " .. name .. "!" }
 end
 
-local function list(req)
-  return names
-end
-
 hive.register("/", hello)
-hive.register("/list", list)
 hive.register("/:name", hello)
