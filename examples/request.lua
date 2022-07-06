7b3f0319d4b7b750dd0f6734e2231178db987729
Eric Long <i@hack3r.moe>
2022-07-06 13:24:58 +0800

    Fix examples

--- examples/request.lua
+++ examples/request.lua
@@ -4,8 +4,7 @@ abel.register("/", function(req)
   local resp = http.request "https://httpbin.org/get"
 
   return {
-    status = resp.status,
+    resp_status = resp.status,
     result = resp.body:parse_json(),
-    h = req.headers["\x00"]
   }
 end)
