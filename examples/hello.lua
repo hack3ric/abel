b339b0126e4cdd5358a767345d9f881a9e321297
Eric Long <i@hack3r.moe>
2022-07-29 20:05:55 +0800

    Actions: fix

--- examples/hello.lua
+++ examples/hello.lua
@@ -1,4 +1,16 @@
+---@diagnostic disable: undefined-global
+local function sleep_print(ms)
+  abel.sleep(ms)
+  print("Sleep " .. ms .. "ms")
+end
+
 local function hello(req)
+  abel.spawn(
+    abel.await_all,
+    bind(sleep_print, 1000),
+    bind(sleep_print, 2000)
+  )
+
   local name = req.params.name or "world"
   return { greeting = "Hello, " .. name .. "!" }
 end
